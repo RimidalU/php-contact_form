999ce13c620f243a93407018df2b1ee9e5069da8
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-26 02:34:19 +0300

    feat: add header component

---
+++ components/Header.php
@@ -0,0 +1,14 @@
+<div class="container-fluid bg-light">
+    <header class="blog-header container py-3 ">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-4 text-center">
+                <a class="blog-header-logo text-dark" href="#">PHP contact form</a>
+            </div>
+            <div class="col-4 pt-1">
+                <?php
+                echo "<a class=\"text-muted\" href=\"$link\">$linkName</a>"
+                ?>
+            </div>
+        </div>
+    </header>
+</div>
