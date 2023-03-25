89bb4ca6770936fe233d9494e1574e6d61e6f945
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-26 02:18:38 +0300

    feat: add footer component

---
+++ components/Footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid bg-light">
+    <footer class="blog-footer py-3">
+        <p>
+            <a href="https://github.com/RimidalU" target="_blank" class="">
+                RimidalU &copy; 2023
+            </a>
+        </p>
+    </footer>
+</div>
