872c440a3a690d75c04cff61e04fcebd4561558c
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-26 11:28:06 +0300

    refactor: change names in form

--- index.php
+++ index.php
@@ -25,28 +25,30 @@
                     </h3>
                     <form action="#" method="post" id="userSettings" enctype="multipart/form-data" class="border p-2">
                         <div class="form-group mb-2">
-                            <label for="inputName"> Name:</label>
-                            <input type="text" class="form-control" id="inputName" name="inputName" aria-describedby="nameHelp" minlength="3" maxlength="50" placeholder="John Doe">
+                            <label for="name"> Name:</label>
+                            <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" minlength="3" maxlength="50" placeholder="John Doe">
                             <small id="namelHelp" class="form-text text-muted">*Enter You name (3 to 50 characters)</small>
                         </div>
                         <div class="form-group mb-2">
                             <label for="gender"> Gender:</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gender" id="male" value="male" checked>
-                                <label class="form-check-label" for="male">
-                                    Male
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gender" id="female" value="female">
-                                <label class="form-check-label" for="female">
-                                    Female
-                                </label>
+                            <div class="row ml-5">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="gender" id="male" value="male" checked>
+                                    <label class="form-check-label" for="male">
+                                        Male
+                                    </label>
+                                </div>
+                                <div class="form-check ml-2">
+                                    <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+                                    <label class="form-check-label" for="female">
+                                        Female
+                                    </label>
+                                </div>
                             </div>
                             <small id="genderHelp" class="form-text text-muted">*Choose you gender</small>
                         </div>
                         <div class="form-group mb-2">
-                            <div class="form-check form-check-inline">
+                            <div class="form-check form-check-inline ml-5">
                                 <input class="form-check-input" type="checkbox" id="cat" value="cat">
                                 <label class="form-check-label" for="cat">Cat</label>
                             </div>
@@ -60,27 +62,27 @@
                             </div>
                             <small id="pet" class="form-text text-muted">*Do you have a pet?</small>
                         </div>
-                            <div class="form-group mb-2">
-                            <label for="inputEmail">Email:</label>
-                            <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp" placeholder="example@email.com" required>
+                        <div class="form-group mb-2">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="example@email.com" required>
                             <small id="emailHelp" class="form-text text-muted">*Enter You name</small>
                         </div>
                         <div class="form-group mb-2">
-                            <label for="inputPas">Password:</label>
-                            <input type="password" class="form-control" id="inputPass" name="inputPass" aria-describedby="passHelp" minlength="6" maxlength="32" placeholder="123456" required>
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" id="password" name="password" aria-describedby="passwordHelp" minlength="6" maxlength="32" placeholder="123456" required>
                             <small id="passHelp" class="form-text text-muted">*Enter Password (6 to 32 characters)</small>
                         </div>
                         <div class="form-group mb-2">
-                            <label for="inputConfirmPass">Confirm password:</label>
-                            <input type="password" class="form-control" id="inputConfirmPass" name="inputConfirmPass" aria-describedby="confirmPassHelp" minlength="6" maxlength="32" placeholder="12345" required>
-                            <small id="confirmPassHelp" class="form-text text-muted">*Confirm the password</small>
+                            <label for="confirmPassword">Confirm password:</label>
+                            <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" aria-describedby="confirmPasswordHelp" minlength="6" maxlength="32" placeholder="123456" required>
+                            <small id="confirmPasswordHelp" class="form-text text-muted">*Confirm the password</small>
                         </div>
                         <div class="form-group mb-2">
                             <div class="input-group mb-1">
                                 <div class="input-group-prepend">
-                                    <label class="input-group-text" for="inputCountry" aria-describedby="countryHelp">Country</label>
+                                    <label class="input-group-text" for="country" aria-describedby="countryHelp">Country</label>
                                 </div>
-                                <select class="custom-select" id="inputCountry" name="inputCountry">
+                                <select class="custom-select" id="country" name="country">
                                     <option selected>Choose...</option>
                                     <option value="Shangri-La">Shangri-La</option>
                                     <option value="Narnia">Narnia</option>
@@ -90,14 +92,14 @@
                             <small id="countryHelp" class="form-text text-muted">*Choose your country</small>
                         </div>
                         <div class="form-group mb-2">
-                            <label for="inputAboutMe" class="form-label">About me:</label>
-                            <textarea class="form-control" id="inputAboutMe" rows="2" placeholder="We all love to be heard. Tell us something about yourself."></textarea>
-                            <small id="inputAboutMe" class="form-text text-muted">*Enter text</small>
+                            <label for="aboutMe" class="form-label">About me:</label>
+                            <textarea class="form-control" id="aboutMe" rows="2" placeholder="We all love to be heard. Tell us something about yourself."></textarea>
+                            <small id="aboutMe" class="form-text text-muted">*Enter text</small>
                         </div>
                         <div class="form-group mb-2">
-                            <label for="inputAvatar" class="form-label">Upload your avatar:</label>
-                            <input class="form-control" type="file" id="inputAvatar">
-                            <small id="inputAvatar" class="form-text text-muted">*Upload a picture or photo</small>
+                            <label for="avatar" class="form-label">Upload your avatar:</label>
+                            <input class="form-control" type="file" id="avatar">
+                            <small id="avatar" class="form-text text-muted">*Upload a picture or photo</small>
                         </div>
                         <div class="container text-end">
                             <input type="reset" class="btn btn-secondary mr-2">
